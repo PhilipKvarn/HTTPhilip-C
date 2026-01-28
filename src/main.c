1168d3af789b222fbbfd89a132b400f03b3ed3cb
philip <phikva@chalmers.se>
2026-01-28 16:53:47 +0100

    initial commit

---
+++ src/main.c
@@ -0,0 +1,72 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <string.h>
+
+
+#define PORT 8080
+
+int main(){
+
+    int server_fd, new_socket;
+
+    struct sockaddr_in address;
+
+    int opt = 1;
+
+    
+
+    int address_len = sizeof(address);
+    char buffer[40000] = {0};
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == 0) {
+        perror("Socket failure");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    if (bind(server_fd, (struct sockaddr *) &address, address_len)) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    listen(server_fd, 3);
+    printf("Listening to port 8080....\n");
+    while (true) {
+        printf("Waiting for connection...\n");
+        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&address_len);
+        if (new_socket < 0) {
+            perror("Accept failed.\n");
+            continue;
+        }
+        memset(buffer, 0, sizeof(buffer));
+        printf("New Connection!\n");
+
+        read(new_socket, buffer, 40000);
+        printf("%s\n",buffer);
+        const char *response = 
+            "HTTP/1.1 200 OK\r\n"
+            "Context-Type: text/plain\r\n"
+            "Content-Length: 12\r\n"
+            "\r\n"
+            "MessageHere\r\n";
+        
+        write(new_socket, response, strlen(response));
+        close(new_socket);
+    }
+    
+    close(server_fd);
+    return 0;   
+}
